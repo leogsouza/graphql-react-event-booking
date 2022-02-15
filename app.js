b66c5c9d51647ea725775cbc6892635df39ff345
Leonardo Souza <leogsouza@gmail.com>
2022-02-14 22:21:31 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+
+const events = [];
+
+app.use(express.json());
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+         
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+        },
+        createEvent: (args) => {
+            const event = {
+                _id: Math.random().toString(),
+                title: args.title,
+                description: args.description,
+                price: args.price,
+                date: new Date(args.date).toISOString(),
+            }
+        }
+    },
+    graphiql: true
+}))
+
+app.listen(3000)
