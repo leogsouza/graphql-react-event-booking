e8c6c07ad47240ce922adbf30b622a73b24b2c8c
Leonardo Souza <leogsouza@gmail.com>
2022-02-17 11:17:49 -0300

    Add Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,25 @@ import { BrowserRouter as Router, Route, Navigate, Routes } from 'react-router-d
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-      <Route path="/" exact element={<Navigate to="/auth" />} />
-      <Route path='/auth' element={<AuthPage />} />
-      <Route path='/events' element={<EventsPage />} />
-      <Route path='/bookings' element={<BookingsPage />} />
-      </Routes>
+      <React.Fragment>
+        <MainNavigation />
+        <main className='main-content'>
+          <Routes>
+            <Route path='/' exact element={<Navigate to='/auth' />} />
+            <Route path='/auth' element={<AuthPage />} />
+            <Route path='/events' element={<EventsPage />} />
+            <Route path='/bookings' element={<BookingsPage />} />
+          </Routes>
+        </main>
+      </React.Fragment>
+      
       
     </Router>
   );
