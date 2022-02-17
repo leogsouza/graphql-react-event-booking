17f5517950c40e049b52c12d8a379c315b6ad99a
Leonardo Souza <leogsouza@gmail.com>
2022-02-17 15:59:09 -0300

    Accessing the User API

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
+})
+
 app.use(isAuth);
 
 app.use('/graphql', graphqlHTTP({
@@ -20,7 +31,7 @@ app.use('/graphql', graphqlHTTP({
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.guiog.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
 .then(() => {
-    app.listen(3000);
+    app.listen(8000);
 })
 .catch(err => {
     console.log(err);
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AuthPage = () => {
+import './Auth.css';
+
+const AuthPage = (props) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleEmailChange = (evt) => {
+    setEmail(evt.target.value);
+  }
+
+  const handlePasswordChange = (evt) => {
+    setPassword(evt.target.value);
+  }
+
+  const switchModeHandler = () => {
+    setIsLogin(!isLogin);
+  }
+
+  const submitHandler = async (evt) => {
+    evt.preventDefault();
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    }
+
+    if (!isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {email: "${email}", password:"${password}"}){
+              _id
+              email
+            }
+          }
+        `
+      };
+    }
+
+    try {
+      const response = await fetch('http://localhost:8000/graphql', {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (response.status !== 200 && response.status !== 201) {
+        throw new Error('Failed!');
+      }
+      return response.json()
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+
   return (
-    <h1>The Auth Page</h1>
+    <form className="auth-form" onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="email">E-mail</label>
+        <input type="email" id="email" value={email} onChange={handleEmailChange} />
+      </div>
+      <div className="form-control">
+      <label htmlFor="password">Password</label>
+        <input type="password" id="password" value={password} onChange={handlePasswordChange} />
+      </div>
+      <div className="form-actions">
+        <button type="submit">Submit</button>
+        <button type="button" onClick={switchModeHandler}>Switch to {isLogin ? 'Signup' : 'Login'}</button>
+      </div>
+    </form>
+    
   )
 }
 
